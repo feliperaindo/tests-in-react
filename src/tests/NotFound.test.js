2ab370db58b36eb316f484825b0eaa7c8f8ef820
Felipe <felipe.raindo@gmail.com>
2022-12-12 16:30:33 -0300

    requisito 4 completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import { act } from 'react-dom/test-utils';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+function tryAccessToInexistencePage() {
+  const page = renderWithRouter(<App />);
+  const NotFoundPage = act(() => page.history.push('/xablau'));
+  return NotFoundPage;
+}
+
+describe('Sequência de testes relacionadas à página `Not Found`', () => {
+  test('Verifica se existe um cabeçalho com texto específico ao tentar acesso à uma página inexistente', () => {
+    tryAccessToInexistencePage();
+
+    const captureHead = screen.getByRole('heading', { name: /Page requested not found/, level: 2 });
+    expect(captureHead).toBeInTheDocument();
+  });
+
+  test('Verifica se existe uma imagem específica ao tentar acesso à uma página inexistenete', () => {
+    tryAccessToInexistencePage();
+
+    const captureImage = screen.getByAltText(/^Pikachu crying because the page requested was not found$/);
+    expect(captureImage).toBeInTheDocument();
+    expect(captureImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
